3ecdad2677d26c499898bbea9845595cef887ac0
Austin L. Howard <contact@austinlhoward.com>
2026-03-28 20:41:00 -0600

    Implement PPU status register read behavior

--- PPU.cpp
+++ PPU.cpp
@@ -25,7 +25,7 @@ uint8_t PPU::cpuRead(uint16_t addr, bool bReadOnly) {
 		break;
 	case 0x0002: // Status
 		data = (status & 0xE0) | (dataBuffer & 0x1F);
-		status &= ~0x80;
+		status &= ~0x80; // Clear vblank flag on read
 		bAddressLatch = false;
 		break;
 	case 0x0003: // OAM Address
@@ -33,17 +33,12 @@ uint8_t PPU::cpuRead(uint16_t addr, bool bReadOnly) {
 	case 0x0004: // OAM Data
 		break;
 	case 0x0005: // Scroll
+		bAddressLatch = false;
 		break;
-	case 0x0006: // PPU Address
+	case 0x0006: // Address
 		break;
-	case 0x0007: // PPU Data
+	case 0x0007: // Data
 		data = dataBuffer;
-		dataBuffer = ppuRead(vramAddr);
-
-		if (vramAddr >= 0x3F00)
-			data = dataBuffer;
-
-		vramAddr += (control & 0x04) ? 32 : 1;
 		break;
 	}
 
@@ -61,92 +56,38 @@ void PPU::cpuWrite(uint16_t addr, uint8_t data) {
 	case 0x0002: // Status
 		break;
 	case 0x0003: // OAM Address
+		oamAddr = data;
 		break;
 	case 0x0004: // OAM Data
+		oamData[oamAddr] = data;
 		break;
 	case 0x0005: // Scroll
-		bAddressLatch = !bAddressLatch;
-		break;
-	case 0x0006: // PPU Address
-		if (!bAddressLatch) {
-			vramAddr = (uint16_t)((data & 0x3F) << 8) | (vramAddr & 0x00FF);
-			bAddressLatch = true;
+		if (bAddressLatch == 0) {
+			fineX = data & 0x07;
+			scrollX = data;
+			bAddressLatch = 1;
 		} else {
-			vramAddr = (vramAddr & 0xFF00) | data;
-			bAddressLatch = false;
+			scrollY = data;
+			bAddressLatch = 0;
 		}
 		break;
-	case 0x0007: // PPU Data
-		ppuWrite(vramAddr, data);
-		vramAddr += (control & 0x04) ? 32 : 1;
+	case 0x0006: // Address
+		if (bAddressLatch == 0) {
+			ppuAddress = (data & 0x3F) << 8; // Only allow access to 14 bits
+			bAddressLatch = 1;
+		} else {
+			ppuAddress |= data;
+			bAddressLatch = 0;
+		}
 		break;
-	}
-}
-
-uint8_t PPU::ppuRead(uint16_t addr, bool bReadOnly) {
-	uint8_t data = 0x00;
-	addr &= 0x3FFF;
-
-	if (addr >= 0x0000 && addr <= 0x1FFF) {
-		data = patternTable[(addr & 0x1000) >> 12][addr & 0x0FFF];
-	}
-	else if (addr >= 0x2000 && addr <= 0x3EFF) {
-		addr &= 0x0FFF;
-		data = nameTable[0][addr & 0x03FF];
-	}
-	else if (addr >= 0x3F00 && addr <= 0x3FFF) {
-		addr &= 0x001F;
-		if (addr == 0x0010) addr = 0x0000;
-		if (addr == 0x0014) addr = 0x0004;
-		if (addr == 0x0018) addr = 0x0008;
-		if (addr == 0x001C) addr = 0x000C;
-		data = paletteTable[addr];
-	}
-
-	return data;
-}
-
-void PPU::ppuWrite(uint16_t addr, uint8_t data) {
-	addr &= 0x3FFF;
-
-	if (addr >= 0x0000 && addr <= 0x1FFF) {
-		patternTable[(addr & 0x1000) >> 12][addr & 0x0FFF] = data;
-	}
-	else if (addr >= 0x2000 && addr <= 0x3EFF) {
-		addr &= 0x0FFF;
-		nameTable[0][addr & 0x03FF] = data;
-	}
-	else if (addr >= 0x3F00 && addr <= 0x3FFF) {
-		addr &= 0x001F;
-		if (addr == 0x0010) addr = 0x0000;
-		if (addr == 0x0014) addr = 0x0004;
-		if (addr == 0x0018) addr = 0x0008;
-		if (addr == 0x001C) addr = 0x000C;
-		paletteTable[addr] = data;
+	case 0x0007: // Data
+		nameTable[((ppuAddress & 0x0C00) >> 10)][ppuAddress & 0x03FF] = data;
+		ppuAddress++; // TODO: Implement address increment based on control register
+		break;
 	}
 }
 
 void PPU::clock() {
-	// set vblank flag at scanline 241, cycle 1
-	if (scanline == 241 && cycle == 1) {
-		status |= 0x80;
-		if (control & 0x80) {
-			bNmi = true;
-		}
-	}
 
-	// pre-render scanline clears vblank
-	if (scanline == -1 && cycle == 1) {
-		status &= ~0x80;
-	}
 
-	cycle++;
-	if (cycle >= 341) {
-		cycle = 0;
-		scanline++;
-		if (scanline >= 261) {
-			scanline = -1;
-			bFrameComplete = true;
-		}
-	}
 }
