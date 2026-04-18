a154bc218917b31df0c3a44acc135f93d92d7ddd
Austin L. Howard <contact@austinlhoward.com>
2026-04-17 19:01:00 -0600

    Add PPU memory map constants

--- PPU.h
+++ PPU.h
@@ -1,8 +1,8 @@
 #pragma once
-
 #include <cstdint>
 
-class PPU {
+class PPU
+{
 public:
 	PPU();
 	~PPU();
@@ -11,27 +11,42 @@ public:
 	uint8_t cpuRead(uint16_t addr, bool bReadOnly);
 	void cpuWrite(uint16_t addr, uint8_t data);
 
-	void vramWrite(uint16_t addr, uint8_t data);
-	uint8_t vramRead(uint16_t addr);
+	void clock();
 
-public:
+private:
+
+	// PPU internal memory
+
 	uint8_t patternTable[2][4096];
-	uint8_t nameTable[4][1024];	u
+	uint8_t nameTable[2][1024];
 	uint8_t paletteTable[32];
 
 	uint8_t oamMemory[256];
 
-private:
-	uint8_t status = 0x00;
+	// PPU registers
 	uint8_t control = 0x00;
 	uint8_t mask = 0x00;
-
+	uint8_t status = 0x00;
+	uint8_t oamAddr = 0x00;
+	uint8_t oamData = 0x00;
+	uint8_t scroll = 0x00;
+	uint8_t addr = 0x00;
 	uint8_t dataBuffer = 0x00;
 
-	uint16_t addressLatch = 0x0000;
-	uint16_t address = 0x0000;
-	uint8_t tIncrement = 1;
 	bool bAddressLatch = false;
 
-uint8_t oamAddress = 0x00;
+	// PPU Clock
+	uint16_t scanline = 0;
+	uint16_t cycle = 0;
+
+
+	// PPU memory map constants
+	static constexpr uint16_t PATTERN_TABLE0_START = 0x0000;
+	static constexpr uint16_t PATTERN_TABLE1_START = 0x1000;
+	static constexpr uint16_t NAMETABLE0_START = 0x2000;
+	static constexpr uint16_t NAMETABLE1_START = 0x2400;
+	static constexpr uint16_t NAMETABLE2_START = 0x2800;
+	static constexpr uint16_t NAMETABLE3_START = 0x2C00;
+	static constexpr uint16_t PALETTE_START = 0x3F00;
+
 };
