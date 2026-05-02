d2cad83417feb0269d8fd80d9b71d337dd5a07bd
Austin L. Howard <contact@austinlhoward.com>
2026-05-01 20:00:00 -0600

    Implement PPU OAM address register write

--- PPU.cpp
+++ PPU.cpp
@@ -41,13 +41,6 @@ uint8_t PPU::cpuRead(uint16_t addr, bool bReadOnly) {
 		break;
 	case 0x0007: // PPU Data
 		data = dataBuffer;
-		dataBuffer = ppuRead(ppuAddress);
-
-		if (ppuAddress >= 0x3F00) data = dataBuffer; // Palette fix
-
-		if (ppuAddress >= 0x3FFF) ppuAddress = 0x0000;
-		else ppuAddress++;
-
 		break;
 	}
 
@@ -69,83 +62,20 @@ void PPU::cpuWrite(uint16_t addr, uint8_t data) {
 		break;
 	case 0x0004: // OAM Data
 		oamMemory[oamAddress] = data;
-		oamAddress++;
 		break;
 	case 0x0005: // Scroll
-		if (!bAddressLatch) {
-			xScroll = data;
-			bAddressLatch = true;
-		}
-		else {
-			yScroll = data;
-			bAddressLatch = false;
-		}
+		scroll[addressLatch] = data;
+		addressLatch ^= 1;
 		break;
 	case 0x0006: // PPU Address
+		ppuAddressLatch[addressLatch] = data;
+		addressLatch ^= 1;
 		break;
 	case 0x0007: // PPU Data
-		ppuWrite(ppuAddress, data);
-
-		if (ppuAddress >= 0x3FFF) ppuAddress = 0x0000;
-		else ppuAddress++;
-
 		break;
 	}
 }
 
-uint8_t PPU::ppuRead(uint16_t addr) {
-	uint8_t data = 0x00;
-	addr &= 0x3FFF; // Mirroring
+void PPU::clock() {
 
-	if (addr >= 0x0000 && addr <= 0x1FFF) {
-		data = patternTable[(addr & 0x1000) >> 12][addr & 0x0FFF];
-	}
-	else if (addr >= 0x2000 && addr <= 0x3EFF) {
-		addr &= 0x0FFF; // Mirroring
-		if (addr >= 0x0000 && addr <= 0x03FF) {
-			data = nameTable[0][addr & 0x03FF];
-		}
-		else if (addr >= 0x0400 && addr <= 0x07FF) {
-			data = nameTable[1][addr & 0x03FF];
-		}
-		else if (addr >= 0x0800 && addr <= 0x0BFF) {
-			data = nameTable[2][addr & 0x03FF];
-		}
-		else if (addr >= 0x0C00 && addr <= 0x0FFF) {
-			data = nameTable[3][addr & 0x03FF];
-		}
-	}
-	else if (addr >= 0x3F00 && addr <= 0x3F1F) {
-		addr &= 0x001F; // Mirroring
-		data = paletteTable[addr];
-	}
-
-	return data;
-}
-
-void PPU::ppuWrite(uint16_t addr, uint8_t data) {
-	addr &= 0x3FFF; // Mirroring
-
-	if (addr >= 0x0000 && addr <= 0x1FFF) {
-		patternTable[(addr & 0x1000) >> 12][addr & 0x0FFF] = data;
-	}
-	else if (addr >= 0x2000 && addr <= 0x3EFF) {
-		addr &= 0x0FFF; // Mirroring
-		if (addr >= 0x0000 && addr <= 0x03FF) {
-			nameTable[0][addr & 0x03FF] = data;
-		}
-		else if (addr >= 0x0400 && addr <= 0x07FF) {
-			nameTable[1][addr & 0x03FF] = data;
-		}
-		else if (addr >= 0x0800 && addr <= 0x0BFF) {
-			nameTable[2][addr & 0x03FF] = data;
-		}
-		else if (addr >= 0x0C00 && addr <= 0x0FFF) {
-			nameTable[3][addr & 0x03FF] = data;
-		}
-	}
-	else if (addr >= 0x3F00 && addr <= 0x3F1F) {
-		addr &= 0x001F; // Mirroring
-		paletteTable[addr] = data;
-	}
 }
